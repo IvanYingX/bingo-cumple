3b9be0819353d87d30c8f31f7a4beddf6b46e5fc
IvanYingX <ying.yingxuan@hotmail.com>
2025-05-16 12:08:31 +0200

    Testtt

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,5 @@ export const API_SLICE_BASE_URL =
   process.env.NODE_ENV === 'production'
     ? 'https://bingo-cumple.vercel.app/api'
     : 'http://localhost:3000/api';
+
+console.log("API_SLICE_BASE_URL", API_SLICE_BASE_URL);
