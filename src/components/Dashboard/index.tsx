fddf9eb6b0f24cc2b1b8d56e10d709b1b5a09bda
Ivan Ying <ying.yingxuan@hotmail.com>
2025-05-30 03:29:16 +0200

    REmoves double quotes

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -15,7 +15,7 @@ export default async function Dashboard() {
         <li>Los jugadores deben marcar los eventos en sus cartones a medida que un evento ocurre.</li>
         <li>Los cartones se generan aleatoriamente y tienen 16 eventos únicos cada uno.</li>
         <li>Los eventos NO pueden ser forzados, deben ocurrir naturalmente durante el juego.</li>
-        <li>Por ejemplo, si un evento dice "Se habla de futbol", hablar uno mismo de futbol no cuenta. Asimismo, no se puede forzar una conversación sobre futbol para que cuente como un evento.</li>
+        <li>Por ejemplo, si un evento dice &quot;Se habla de futbol&quot;, hablar uno mismo de futbol no cuenta. Asimismo, no se puede forzar una conversación sobre futbol para que cuente como un evento.</li>
         <li>Salir de un juego no afecta al progreso del jugador.</li>
         <li>El juego se puede reiniciar las veces que se quiera.</li>
       </ul>
